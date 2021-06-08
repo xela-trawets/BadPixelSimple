51501af654172fddc83b5fb05fcb5e19f9f39c9a
ideal.light <ideal.light@outlook.com>
2021-06-08 03:04:00 -0700

    BPL class etc

---
+++ BadPixelSimpleApp/BadPixelListClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadPixelSimpleApp
+{
+    public record BadPixel(int RawX, int RawY);
+    public record BadRow(int RawY);
+    public record BadColumn(int RawX);
+    public record BadPixelList(string DetectorId)
+    {
+        public static List<BadPixel> BadPixels { set; get; } = new();
+        public static List<BadRow> BadRows { set; get; } = new();
+        public static List<BadColumn> BadColumns { set; get; } = new();
+    }
+}

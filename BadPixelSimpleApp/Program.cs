3193d3dead0d12807c5804e5ee35aa27b32f20a9
ideal.light <ideal.light@outlook.com>
2021-08-27 23:15:24 -0700

    added inverse scan, det config parser

--- BadPixelSimpleApp/Program.cs
+++ BadPixelSimpleApp/Program.cs
@@ -7,19 +7,116 @@ using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace BadPixelSimpleApp
 {
     class Program
-    {/// <summary>
-     /// This program reads a file specifying bad pixels
-     /// The output file is the binary bitstream to be stored on the detector
-     /// </summary>
-     /// <param name="args">Json input file See Test.Json for example</param>
+    {
+        public static class PcrTestImplementation
+        {
+            const int bitsPerAsic = 557184;
+            const int bytesPerAsic = ((bitsPerAsic + 31) / 32) * 4;
+            const int pcrByteSize = bytesPerAsic * asicCount;
+            const int asicCount = 8;
+            static void setbit(Span<byte> bytes, int bitPos)
+            {
+                bytes[bitPos / 8] |= (byte)(128 >> (bitPos % 8));
+            }
+            public static int getbit(Span<byte> bytes, int bitPos)
+            {
+                int t = bytes[bitPos / 8] & (byte)(128 >> (bitPos % 8));
+                return t == 0 ? 0 : 1;
+            }
+            public static int PixBitPos(int nBit, int x, int y) =>
+                (x / 128) * bitsPerAsic +
+                (255 - y) * 17 * 128 +
+                nBit * 128 + (x % 128);
+            public static int KillPixBitPos(int x, int y) =>
+                PixBitPos(14, x, y);
+            //(x / 128) * bitsPerAsic +
+            //(255 - y) * 17 * 128 +
+            //14 * 128 + (x % 128);
+            public static void KillPix(byte[] pcrBytes, int x, int y)
+            {
+                int bitPos = KillPixBitPos(x, y);
+                setbit(pcrBytes, bitPos);
+            }
+            public static int GetPcrXY(byte[] pcrBytes, int x, int y)
+            {
+                int result = 0;
+                for (int nBit = 0; nBit < 17; nBit++)
+                {
+                    int bitPos = PixBitPos(nBit, x, y);
+                    int b = getbit(pcrBytes, bitPos) << nBit;
+                    result = result | b;
+                }
+                return result;
+            }
+        }
+        static void ReadPcrTest()
+        {
+            var detcfgLines = File.ReadAllLines(@"C:\Users\AlexA\Downloads\mca604\media\card\apps\detector.config", Encoding.ASCII)
+                .Select(lx => lx.Trim().Split('=').Select(wx => wx.Trim()))
+                .Where(wxl => wxl.Count() == 2)
+                //.Where(wxl => int.TryParse(wxl.ElementAt(1),out int tmp))
+                .Select(wxl => new KeyValuePair<string, string>(wxl.ElementAt(0), wxl.ElementAt(1)))
+                .ToList();
+
+            var detCfg = new Dictionary<string, string>(detcfgLines);
+            var intValuePairs = detCfg.Where(kvp => int.TryParse(kvp.Value, out int tmp))
+                .Select(kvp => new KeyValuePair<string, int>(kvp.Key, int.Parse(kvp.Value)));
+            var detIntCfg = new Dictionary<string, int>(intValuePairs, StringComparer.OrdinalIgnoreCase);
+
+            int w = detIntCfg["number_of_imaging_units"]
+                * detIntCfg["number_of_asics_per_imaging_unit"]
+                * detIntCfg["number_of_asic_cols"];
+            int h = detIntCfg["number_of_asic_rows"];
+            var pcrBytes = File.ReadAllBytes(@"C:\Users\AlexA\Downloads\mca604\media\card\apps\pcr.config");
+
+            //(x / 128) * bitsPerAsic +
+            //(255 - y) * 17 * 128 +
+            //14 * 128 + (x % 128);
+            var bitsSet = Enumerable.Range(557184 * 11, 557184)
+                .Where (bitPos => PcrTestImplementation.getbit(pcrBytes, bitPos)!=0)
+                .Select(t => (
+                a: t / 557184,
+                b: ((t % 557184) % (17 * 128)) / 128,
+                x: (t % 557184) % 128,
+                y: 255 - ((t % 557184) / (17 * 128))
+                ))
+                .ToList();
+
+            //var allZero = pcrBytes.SelectMany((t, n) => Enumerable.Range(0,8).Where(b=>)).Where(t => t.v != 0)
+            //    .Select(t => (a: (t.i * 8) / 557184,
+            //    b: (((t.i * 8) % 557184) % (17 * 128)) / 128,
+            //    x: (((t.i * 8) % 557184) % (17 * 128)) % 128,
+            //    y: 255 - ((t.i * 8) % 557184) / (17 * 128)
+            //    ))
+            //    .ToList();
+            for (int nAsic = 0; nAsic < 16; nAsic++)
+            {
+                for (int y = 80; y < 88; y++)
+                    for (int x = 1240; x < 1243; x++)
+                    {
+                        int t = PcrTestImplementation.GetPcrXY(pcrBytes, x, y);
+                        Console.Write($"{t:D6} ");
+                    }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// This program reads a file specifying bad pixels
+        /// The output file is the binary bitstream to be stored on the detector
+        /// </summary>
+        /// <param name="args">Json input file See Test.Json for example</param>
         static void Main(string[] args)
         {
+            ReadPcrTest();
+            Environment.Exit(0);
             var di = DetConfigReader.DetConfigReader.TelnetDetInfo("192.168.184.130").Result;
             Console.WriteLine(di);
 
